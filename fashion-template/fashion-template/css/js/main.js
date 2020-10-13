8fa6bb41a64a27f3adb18a2651b6159b85845fcb
Shizuka murakami <69420652+omuomu02414@users.noreply.github.com>
2020-10-13 09:49:01 +0900

    Add files via upload

---
+++ fashion-template/fashion-template/css/js/main.js
@@ -0,0 +1,16 @@
+
+    var mySwiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        }
+    });
