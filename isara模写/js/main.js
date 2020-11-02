4e29cd8f6d8b3b21e1a4269ce1170e4a889c8b5c
Shizuka murakami <69420652+omuomu02414@users.noreply.github.com>
2020-11-02 11:30:59 +0900

    Add files via upload

--- isara模写/js/main.js
+++ isara模写/js/main.js
@@ -7,3 +7,14 @@ $(function(){
       $(this).toggleClass("open");
     });
   });
+
+
+  jQuery(function ($) {
+    $('.js-accordion-title').on('click', function () {
+      /*クリックでコンテンツを開閉*/
+      $(this).next().slideToggle(200);
+      /*矢印の向きを変更*/
+      $(this).toggleClass('open', 200);
+    });
+    
+    });
