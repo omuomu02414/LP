577e3d28740c253f43b4df0d56af6b1c01d5ffbe
Shizuka murakami <69420652+omuomu02414@users.noreply.github.com>
2020-11-24 09:15:51 +0900

    Add files via upload

---
+++ isara模写/isara模写/js/main.js
@@ -0,0 +1,84 @@
+// .s_01 .accordion_one
+$(function(){
+    //.accordion_oneの中の.accordion_headerがクリックされたら
+    $('.s_01 .accordion_one .accordion_header').click(function(){
+      //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+      $(this).next('.accordion_inner').slideToggle();
+      $(this).toggleClass("open");
+    });
+  });
+
+
+  jQuery(function ($) {
+    $('.js-accordion-title').on('click', function () {
+      /*クリックでコンテンツを開閉*/
+      $(this).next().slideToggle(200);
+      /*矢印の向きを変更*/
+      $(this).toggleClass('open', 200);
+    });
+    
+    });
+
+
+// グローバル変数
+var syncerTimeout = null ;
+
+// 一連の処理
+$( function()
+{
+	// スクロールイベントの設定
+	$( window ).scroll( function()
+	{
+		// 1秒ごとに処理
+		if( syncerTimeout == null )
+		{
+			// セットタイムアウトを設定
+			syncerTimeout = setTimeout( function(){
+
+				// 対象のエレメント
+				var element = $( '#page-top' ) ;
+
+				// 現在、表示されているか？
+				var visible = element.is( ':visible' ) ;
+
+				// 最上部から現在位置までの距離を取得して、変数[now]に格納
+				var now = $( window ).scrollTop() ;
+
+				// 最下部から現在位置までの距離を計算して、変数[under]に格納
+				var under = $( 'body' ).height() - ( now + $(window).height() ) ;
+
+				// 最上部から現在位置までの距離(now)が1500以上かつ
+				// 最下部から現在位置までの距離(under)が200px以上かつ…
+				if( now > 1500 && 200 < under )
+				{
+					// 非表示状態だったら
+					if( !visible )
+					{
+						// [#page-top]をゆっくりフェードインする
+						element.fadeIn( 'slow' ) ;
+					}
+				}
+
+				// 1500px以下かつ
+				// 表示状態だったら
+				else if( visible )
+				{
+					// [#page-top]をゆっくりフェードアウトする
+					element.fadeOut( 'slow' ) ;
+				}
+
+				// フラグを削除
+				syncerTimeout = null ;
+			} , 1000 ) ;
+		}
+	} ) ;
+
+	// クリックイベントを設定する
+	$( '#move-page-top' ).click(
+		function()
+		{
+			// スムーズにスクロールする
+			$( 'html,body' ).animate( {scrollTop:0} , 'slow' ) ;
+		}
+	) ;
+} ) ;
